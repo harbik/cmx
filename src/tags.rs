fb9fe117548c0272250d754fca139cb0ddb27ecc
Gerard Harbers <harbers@gmail.com>
2022-01-12 20:36:07 -0800

    Lut8Type, and added more test .icc profiles

--- src/tags.rs
+++ src/tags.rs
@@ -1,5 +1,4 @@
 
-use std::str::FromStr;
 
 use crate::{profile::*, tag_signatures::TagSignature};
 use num::{FromPrimitive, Zero};
@@ -30,6 +29,7 @@ pub enum TagData {
     Float32Array(Vec<f32>), // 'fl32'
     Float64Array(Vec<f64>), // 'fl64'
     GamutBoundaryDescription(Vec<u8>), // 'gbd'
+    Lut8(Lut8),
     LutAToB(Vec<u8>), // 'mAB'
     LutBToA(Vec<u8>), // 'mBA'
     Measurement(Measurement), // 'meas'
@@ -93,6 +93,42 @@ pub struct Float32Array(Vec<f32>);
 
 #[derive(Debug, Serialize)]
 pub struct Float64Array(Vec<f64>);
+
+
+#[derive(Debug, Serialize)]
+pub struct Lut8 {
+    pub n: usize, // input channels
+    pub m: usize, // output channels
+    pub k: usize,
+    pub e_mat: Vec<f32>,
+    pub input_lut: Vec<u8>,
+    pub output_lut: Vec<u8>,
+    pub multi_lut: Vec<u8>,
+}
+
+impl Lut8 {
+    pub fn try_new(buf: &mut &[u8]) -> Result<Self, Box<dyn std::error::Error + 'static>> {
+        let n = read_u8(buf)? as usize;
+        let m = read_u8(buf)? as usize;
+        let k = read_u8(buf)? as usize;
+        let _ = read_u8(buf)?; // padding
+        let e_mat = read_s15fixed16_array(buf,36.into())?;
+        let input_lut = read_vec(buf, (n*256).into())?;
+        let n_i32 = (n as i32).try_into()?;
+        let multi_lut = read_vec(buf, (k.pow(n_i32)*m).into())?;
+        let output_lut = read_vec(buf, (m*256).into())?;
+        Ok(Lut8 {
+            n,
+            m,
+            k,
+            e_mat,
+            input_lut,
+            output_lut,
+            multi_lut,
+        })
+    }
+}
+
 
 #[derive(Debug, Serialize)]
 pub struct Measurement {
@@ -333,6 +369,9 @@ impl TagData {
                     v.push(read_be_f64(buf)?)
                 }
                 Ok(Self::Float64Array(v))
+            },
+            (_, TagTypeSignature::Lut8Type) => {
+                Ok(Self::Lut8(Lut8::try_new(buf)?))
             },
             (_, TagTypeSignature::MeasurementType) => {
                 Ok(Self::Measurement(Measurement::try_new(buf)?))
